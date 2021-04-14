cfbba94c890fc7ab6f781676f99c13d2eed81de9
whachiragorn <whachiragorn.ton@student.mahidol.edu>
2021-04-14 13:34:13 +0700

    Compare LastUpdated in text file

--- Covid19.py
+++ Covid19.py
@@ -1,16 +1,27 @@
 import requests
-r = requests.get('https://covid19.th-stat.com/api/open/today')
-print("LastUpdate",r.json()["UpdateDate"])
-print("Confirmed",r.json()["Confirmed"])
-print("NewConfirmed",r.json()["NewConfirmed"])
-print("NewRecovered",r.json()["NewRecovered"])
-print("NewDeaths",r.json()["NewDeaths"])
+import time
 
 
 # Code for Line Notification
 url = 'https://notify-api.line.me/api/notify'
-token = 'HpXwfTCIngXaZSkwtM27S26girdn9MzGdOWbFUc793h'   # Token from Line Notify
+# token = 'HpXwfTCIngXaZSkwtM27S26girdn9MzGdOWbFUc793h'   # Token from Line Notify (Line Notify)
+token = 'VmkcmkKUxIj653YwNtc1RAKQuaSLvTw0BKJEwIh3v1R ' #ตัวฉันอีกคน
 headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+ token}
+
+
+
+
+f = open("date.txt", "r")
+LastUpdate = f.read()
+print('Last Update in text file : '+LastUpdate)
+
+r = requests.get('https://covid19.th-stat.com/api/open/today')
+print("LastUpdate",r.json()["UpdateDate"])
+UpdateDate = r.json()["UpdateDate"]
+print("Confirmed",r.json()["Confirmed"])
+print("NewConfirmed",r.json()["NewConfirmed"])
+print("NewRecovered",r.json()["NewRecovered"])
+print("NewDeaths",r.json()["NewDeaths"])
 
 msg = "อัพเดทข้อมูลล่าสุด: " + str(r.json()['UpdateDate'])
 msg2 = " ติดเชื้อสะสม: " + str(r.json()['Confirmed'])
@@ -19,12 +30,12 @@ msg4 = " รักษาแล้ว: " + str(r.json()['NewRecovered'])
 msg5 = " ตาย: " + str(r.json()['NewDeaths'])
 msgall = msg+msg2+msg3+msg4+msg5
 
-#r1 = requests.post(url, headers=headers, data = {'message':msg})
-#r2 = requests.post(url, headers=headers, data = {'message':msg2})
-#r3 = requests.post(url, headers=headers, data = {'message':msg3})
-#r4 = requests.post(url, headers=headers, data = {'message':msg4})
-#r5 = requests.post(url, headers=headers, data = {'message':msg5})
+if LastUpdate != UpdateDate:
+    r6 = requests.post(url, headers=headers, data = {'message':msgall})
+    LastUpdate = UpdateDate
+    f = open("date.txt", "w")
+    f.write(str(LastUpdate))
+    f.close()
 
-r6 = requests.post(url, headers=headers, data = {'message':msgall})
 
 #From https://www.youtube.com/watch?v=KXH9KroXl5Y
